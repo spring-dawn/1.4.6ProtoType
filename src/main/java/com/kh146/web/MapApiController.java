fe87a96a902c3fcf303c2a1786a0f2f91c693c7c
Lariatte <one_spring_day94@naver.com>
2022-03-07 00:04:46 +0900

    220306 지도화면ajax 시험

---
+++ src/main/java/com/kh146/web/MapApiController.java
@@ -0,0 +1,14 @@
+package com.kh146.web;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/map")     //뷰 매핑과 구분짓는다. Controller + responseBody
+public class MapApiController {
+
+
+
+}
---
+++ src/main/java/com/kh146/web/MapController.java
@@ -0,0 +1,20 @@
+package com.kh146.web;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping("map")
+public class MapController {
+
+    @GetMapping
+    public String mapViewTest(){
+
+        return "mapViewTest";
+    }
+
+
+}
